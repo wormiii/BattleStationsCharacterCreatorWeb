a421d11b03982781198ffdc8ed51e887aa103705
Bryan Daniels <wormiii@hotmail.com>
2016-01-19 20:04:29 -0500

    save to file

--- js/index.js
+++ js/index.js
@@ -6,7 +6,8 @@ requirejs.config({
         knockout: 'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.0/knockout-min',
         lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.min',
         text: 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
-        jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min'
+        jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min',
+        filesaver: 'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2014-11-29/FileSaver.min'
     }
 });
 
--- js/navigationsection.js
+++ js/navigationsection.js
@@ -1,6 +1,6 @@
 define(
-["knockout"], 
-function(ko) 
+['knockout', 'filesaver'], 
+function(ko, filesaver) 
 {
 
 function navigationSectionViewModel(indexViewModel)
@@ -32,6 +32,15 @@ function navigationSectionViewModel(indexViewModel)
 	{
 		window.open("http://www.etcher2games.com");
 	};
+
+	self.navigationSaveCharacter = function()
+	{
+		var character = self.indexViewModel.character(); 
+		var text = ko.toJSON(character);
+		var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
+  		var filename = "battlestationscharacter_" + character.player() + "_" + character.name() + ".txt";
+  		filesaver(blob, filename);
+ 	};
 }
 
 return navigationSectionViewModel;
