a50ed3dbc9620cf6ec4a2712f9a3ee89917b5dde
Bryan Daniels <wormiii@hotmail.com>
2016-01-25 20:05:03 -0500

    oops

--- js/indexViewModel.js
+++ js/indexViewModel.js
@@ -54,7 +54,7 @@ function indexViewModel()
 
     self.navigationSaveCharacter = function()
     {
-        self.indexViewModel.character().saveToFile(); 
+        self.character().saveToFile(); 
     };
 
     // character sections
