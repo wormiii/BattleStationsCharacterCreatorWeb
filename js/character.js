4ac3206d7204e1bf51e4effdda7af932aeda0e4c
Bryan Daniels <wormiii@hotmail.com>
2016-01-26 18:08:51 -0500

    minor fixes

--- js/character.js
+++ js/character.js
@@ -7,12 +7,12 @@ function characterViewModel()
 {
     var self = this;
 
-    self.player = ko.observable("Bryan");
-    self.name = ko.observable("Hex");
-    self.rank = ko.observable(3);
-    self.credits = ko.observable(4455);
-    self.xp = ko.observable(560);
-    self.prestige = ko.observable(1000);
+    self.player = ko.observable("");
+    self.name = ko.observable("");
+    self.rank = ko.observable(1);
+    self.credits = ko.observable(0);
+    self.xp = ko.observable(0);
+    self.prestige = ko.observable(0);
 
     self.saveToJSON = function()
     {
